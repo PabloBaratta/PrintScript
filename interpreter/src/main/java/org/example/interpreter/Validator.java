98bbc8c89e001095cc5429d11609f5e0ad1e55da
anitaabramowich <aniabramowich2003@gmail.com>
2024-08-20 12:29:57 -0300

    Validator interpreter

--- interpreter/src/main/java/org/example/interpreter/Validator.java
+++ interpreter/src/main/java/org/example/interpreter/Validator.java
@@ -3,15 +3,32 @@ package org.example.interpreter;
 import org.example.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Stack;
 
 public class Validator implements ASTVisitor {
 
     private final Map<String, Variable> environment = new HashMap<>();
+    private final Stack<Expression> stack = new Stack<>();
 
     @Override
     public void visit(Assignation assignation) throws Exception {
         Identifier identifier = assignation.getIdentifier();
-        if (!environment.containsKey(identifier.toString())) {
+        Expression expression = assignation.getExpression();
+        if (environment.containsKey(identifier.toString())) {
+            evaluate(expression);
+            Expression astNodeResult = stack.pop();
+            Variable variable = environment.get(identifier.toString());
+            if (astNodeResult instanceof TextLiteral && variable.getType().getTypeName().equals("string")) {
+                variable.setExpression(astNodeResult);
+                environment.put(identifier.toString(), variable);
+            } else if (astNodeResult instanceof NumericLiteral && variable.getType().getTypeName().equals("number")) {
+                variable.setExpression(astNodeResult);
+                environment.put(identifier.toString(), variable);
+            } else {
+                throw new Exception("los tipos no coinciden");
+            }
+        } else {
             throw new Exception("la variable no esta declarada");
         }
     }
@@ -19,27 +36,48 @@ public class Validator implements ASTVisitor {
     @Override
     public void visit(VariableDeclaration variableDeclaration) throws Exception {
         Identifier identifier = variableDeclaration.getIdentifier();
+        Type type = variableDeclaration.getType();
         if (environment.containsKey(identifier.toString())) {
             throw new Exception("la variable ya esta declarada");
         }
-        environment.put(identifier.toString(), new Variable(variableDeclaration.getType(), null));
+        if (variableDeclaration.getExpression().isPresent()) {
+            evaluate(variableDeclaration.getExpression().get());
+            Expression astNodeResult = stack.pop();
+            if (type.getTypeName().equals("string") && astNodeResult instanceof TextLiteral) {
+                environment.put(identifier.toString(), new Variable(type, astNodeResult));
+            } else if (type.getTypeName().equals("number") && astNodeResult instanceof NumericLiteral) {
+                environment.put(identifier.toString(), new Variable(type, astNodeResult));
+            } else {
+                throw new Exception("los tipos no coinciden");
+            }
+        } else {
+            environment.put(identifier.toString(), new Variable(type, null));
+        }
     }
 
     @Override
     public void visit(Identifier identifier) throws Exception {
-        if (!environment.containsKey(identifier.getName())) {
+        String identifierName = identifier.getName();
+        if (environment.containsKey(identifierName)) {
+            Variable variable = environment.get(identifierName);
+            Optional<Expression> optionalExpression = variable.getExpression();
+            if (optionalExpression.isEmpty()) {
+                throw new Exception("variable was declared but not assigned");
+            }
+            stack.push(optionalExpression.get());
+        } else {
             throw new Exception("undeclared variable");
         }
     }
 
     @Override
     public void visit(TextLiteral textLiteral) {
-        // No validation needed for literals
+        stack.push(textLiteral);
     }
 
     @Override
     public void visit(NumericLiteral numericLiteral) {
-        // No validation needed for literals
+        stack.push(numericLiteral);
     }
 
     @Override
@@ -56,7 +94,33 @@ public class Validator implements ASTVisitor {
 
     @Override
     public void visit(BinaryExpression binaryExpression) throws Exception {
-        // Validate binary expression if needed
+        evaluate(binaryExpression.getLeft());
+        evaluate(binaryExpression.getRight());
+        Expression right = stack.pop();
+        Expression left = stack.pop();
+        if (binaryExpression.getOperator().equals("+")) {
+            if (left instanceof NumericLiteral && right instanceof NumericLiteral) {
+                stack.push(new NumericLiteral(0.0));
+            } else {
+                stack.push(new TextLiteral(""));
+            }
+        } else if (!(left instanceof NumericLiteral && right instanceof NumericLiteral)) {
+            throw new Exception("para las siguientes operaciones tienen que ser numeros");
+        } else {
+            switch (binaryExpression.getOperator()) {
+                case "-":
+                    stack.push(new NumericLiteral(0.0));
+                    break;
+                case "/":
+                    stack.push(new NumericLiteral(0.0));
+                    break;
+                case "*":
+                    stack.push(new NumericLiteral(0.0));
+                    break;
+                default:
+                    throw new Exception("Operador no válido: " + binaryExpression.getOperator());
+            }
+        }
     }
 
     @Override
@@ -71,7 +135,15 @@ public class Validator implements ASTVisitor {
         parenthesis.getExpression().accept(this);
     }
 
+    private void evaluate(ASTNode node) throws Exception {
+        node.accept(this);
+    }
+
     public Map<String, Variable> getEnvironment() {
         return environment;
+    }
+
+    public Stack<Expression> getStack() {
+        return stack;
     }
 }
