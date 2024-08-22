4374cd8f378467ecd1ef7844925950e5b10d2549
Bruno Iacovone <iacovonebru@gmail.com>
2024-08-22 14:49:26 -0300

    second commit

--- cli/src/main/java/org/example/builders/AnalyzingBuilder.java
+++ cli/src/main/java/org/example/builders/AnalyzingBuilder.java
@@ -1,8 +1,31 @@
 package org.example.builders;
 
+import org.example.Program;
+import org.example.lexer.token.Token;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import static org.example.Runner.lex;
+import static org.example.Runner.parse;
+
 public class AnalyzingBuilder implements CommandBuilder{
     @Override
-    public void buildAndRun(String[] parts) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public String buildAndRun(String[] parts) throws Exception {
+        String pathFile = parts[1];
+        String pathConfig = parts[2];
+        if (pathFile == null) {
+            throw new RuntimeException("File path not found");
+        }
+        if (pathConfig == null) {
+            throw new RuntimeException("Config path not found");
+        }
+        String code = Files.lines(Paths.get(pathFile))
+                .collect(Collectors.joining("\n"));
+        List<Token> tokens = lex(code);
+        Program program = parse(tokens);
+        // Analyzing logic
+        // return analyzer.analyze(program); ???
+        return "Analyzing completed";
     }
 }
--- cli/src/main/java/org/example/builders/ValidationBuilder.java
+++ cli/src/main/java/org/example/builders/ValidationBuilder.java
@@ -1,11 +1,19 @@
 package org.example.builders;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 public class ValidationBuilder implements CommandBuilder{
     @Override
-    public void buildAndRun(String[] parts) throws Exception {
+    public String buildAndRun(String[] parts) throws Exception {
         String pathFile = parts[1];
         if (pathFile == null) {
             throw new RuntimeException("File path not found");
         }
+        String code = Files.lines(Paths.get(pathFile))
+                .collect(Collectors.joining("\n"));
+        // Validation logic
+        return "Validation completed";
     }
 }
