8880e0c242caa580b86f870f3241f4e3cb7d8799
Bruno Iacovone <iacovonebru@gmail.com>
2024-08-27 13:42:26 -0300

    cli tests

--- cli/src/main/java/org/example/builders/ExecutionBuilder.java
+++ cli/src/main/java/org/example/builders/ExecutionBuilder.java
@@ -2,16 +2,15 @@ package org.example.builders;
 
 import org.example.Runner;
 
+import java.nio.file.Paths;
+
 public class ExecutionBuilder implements CommandBuilder{
     @Override
     public String buildAndRun(String[] parts) throws Exception {
         if (parts.length != 2) {
             throw new RuntimeException("Invalid number of arguments, should be two");
         }
-        String filePath = parts[1];
-        if (filePath == null) {
-            throw new RuntimeException("File path not found");
-        }
+        String filePath = Paths.get("").toAbsolutePath().getParent() + parts[1];
         Runner.run(filePath);
         return "Execution completed";
     }
---
+++ cli/src/test/java/CliTest.java
@@ -0,0 +1,42 @@
+import org.example.Cli;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CliTest {
+	@Test
+	public void testDoesNotThrow() {
+		Cli cli = new Cli(Paths.get("").toAbsolutePath() + "/src/test/resources/terminalTest.txt");
+		assertDoesNotThrow(cli::run);
+	}
+
+	@Test
+    public void testThrow1() throws IOException {
+        String filePath = Paths.get("").toAbsolutePath() + "/src/test/resources/terminalTest1.txt";
+
+        writeToFile(filePath, "analyze");
+        Cli cli1 = new Cli(filePath);
+        assertThrows(RuntimeException.class, cli1::run);
+
+        writeToFile(filePath, "format");
+        Cli cli2 = new Cli(filePath);
+        assertThrows(RuntimeException.class, cli2::run);
+
+        writeToFile(filePath, "execute");
+        Cli cli3 = new Cli(filePath);
+        assertThrows(RuntimeException.class, cli3::run);
+
+        writeToFile(filePath, "validate");
+        Cli cli4 = new Cli(filePath);
+        assertThrows(RuntimeException.class, cli4::run);
+    }
+
+    private void writeToFile(String path, String content) throws IOException {
+        Files.write(Paths.get(path), content.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+    }
+}
