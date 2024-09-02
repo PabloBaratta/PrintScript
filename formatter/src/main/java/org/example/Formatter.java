952691ace745f410185c620b863821b3e69744a7
Josefina Sapag <jmsapag17@gmail.com>
2024-09-02 15:21:07 -0300

    adding and testing new format cases

--- formatter/src/main/java/org/example/Formatter.java
+++ formatter/src/main/java/org/example/Formatter.java
@@ -29,16 +29,65 @@ public class Formatter {
 			case VariableDeclaration variableDeclaration:
 				result.append(formatVarDec(variableDeclaration));
 				break;
+			case ConstDeclaration constDeclaration:
+				result.append(formatConstDec(constDeclaration));
+				break;
 			case Assignation assignation:
 				result.append(formatAssignation(assignation));
 				break;
 			case Method method:
 				result.append(formatMethod(method));
 				break;
+			case IfStatement ifStatement:
+				result.append(formatIfStatement(ifStatement));
+				break;
 			default:
 				String s = "Unknown node type: ";
 				throw new IllegalArgumentException(s + child.getClass());
 		}
+	}
+
+	private StringBuilder formatIfStatement(IfStatement ifStatement) {
+		Expression condition = ifStatement.getCondition();
+		List<ASTNode> thenBlock = ifStatement.getThenBlock();
+		List<ASTNode> elseBlock = ifStatement.getElseBlock();
+		StringBuilder result = new StringBuilder();
+		result.append("if (").append(condition.toString()).append(") { \n");
+		formatChildren(thenBlock, result);
+		result.append("}");
+		if (elseBlock.isEmpty()){
+			result.append("\n");
+			return result;
+		}
+		result.append(" else { \n");
+		formatChildren(elseBlock, result);
+		result.append("}\n");
+		return result;
+	}
+
+	private void formatChildren(List<ASTNode> thenBlock, StringBuilder result) {
+		for (ASTNode node : thenBlock) {
+			checkSpaces(result);
+			formatNode(node, result);
+		}
+	}
+
+	private StringBuilder formatConstDec(ConstDeclaration constDeclaration) {
+		Identifier identifier = constDeclaration.getIdentifier();
+		Type type = constDeclaration.getType();
+		Expression expression = constDeclaration.getExpression();
+		StringBuilder result = new StringBuilder();
+		result.append("const ").append(identifier.toString());
+		checkRule("spaceBeforeColon", " ", result);
+		result.append(":");
+		checkRule("spaceAfterColon", " ", result);
+		result.append(type.getTypeName());
+		checkRule("spaceBeforeAssignation", " ", result);
+		result.append("=");
+		checkRule("spaceAfterAssignation", " ", result);
+		result.append(expression.toString());
+		result.append(";\n");
+		return result;
 	}
 
 	private StringBuilder formatMethod(Method method) {
@@ -67,6 +116,13 @@ public class Formatter {
 		if (rules.get("newLineBeforePrintln").getRule()) {
 			int qty = rules.get("newLineBeforePrintln").getQty().get();
 			result.append("\n".repeat(Math.max(0, qty)));
+		}
+	}
+
+	private void checkSpaces(StringBuilder result) {
+		if (rules.get("indentation").getRule()) {
+			int qty = rules.get("indentation").getQty().get();
+			result.append(" ".repeat(Math.max(0, qty)));
 		}
 	}
 
