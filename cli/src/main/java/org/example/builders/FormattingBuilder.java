af5896ec78a4b4fae8d8bd1ab44f3a442f20a3e9
Josefina Sapag <jmsapag17@gmail.com>
2024-09-09 21:12:50 -0300

    fixing merge conflicts

--- cli/src/main/java/org/example/builders/FormattingBuilder.java
+++ cli/src/main/java/org/example/builders/FormattingBuilder.java
@@ -2,18 +2,15 @@ package org.example.builders;
 
 import org.example.*;
 import org.example.lexer.StreamReader;
-import org.example.lexer.token.Token;
 import org.example.lexer.Lexer;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.example.Runner.parse;
 import static org.example.lexer.LexerProvider.provideV10;
 
 public class FormattingBuilder implements CommandBuilder{
@@ -23,16 +20,13 @@ public class FormattingBuilder implements CommandBuilder{
             throw new RuntimeException("Invalid number of arguments, should be three");
         }
         String pathFile = Paths.get("").toAbsolutePath() + parts[1];
-        String pathConfig = Paths.get("").toAbsolutePath() + parts[2];
         String code = Files.lines(Paths.get(pathFile))
                 .collect(Collectors.joining("\n"));
         InputStream inputStream = new ByteArrayInputStream(code.getBytes());
         StreamReader reader = new StreamReader(inputStream);
         Lexer lexer = provideV10(reader);
-        Program program = parse(lexer);
-        Map<String, Rule> rules = readJson(pathConfig);
-        PrintScriptIteratorTest<ASTNode> nodes = new PrintScriptIteratorTest<>(program.getChildren());
-        Formatter formatter = FormatterProvider.provideV10();
+        Parser parser = ParserProvider.provide10(lexer);
+        Formatter formatter = FormatterProvider.provideV10(parser);
         System.out.println(formatter.format());
         return formatter.format();
     }
--- formatter/src/main/java/org/example/FormatterProvider.java
+++ formatter/src/main/java/org/example/FormatterProvider.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 public class FormatterProvider {
 
-	public static Formatter provideV10() throws IOException {
-		return new Formatter(Ruler.rulesV10(), new PrintScriptIteratorTest<ASTNode>(List.of()));
+	public static Formatter provideV10(PrintScriptIterator<ASTNode> nodes) throws IOException {
+		return new Formatter(Ruler.rulesV10(), nodes);
 	}
 
-	public static Formatter provideV11() throws IOException {
-		return new Formatter(Ruler.rulesV11(), new PrintScriptIteratorTest<ASTNode>(List.of()));
+	public static Formatter provideV11(PrintScriptIterator<ASTNode> nodes) throws IOException {
+		return new Formatter(Ruler.rulesV11(), nodes);
 	}
 
 }
